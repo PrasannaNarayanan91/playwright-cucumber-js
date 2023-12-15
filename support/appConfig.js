a4e2492f132045bf87331a1281f29de8bb9b235a
PrasannaNarayanan91 <prasa91@gmail.com>
2023-12-15 19:00:46 +1100

    fix updates

--- support/appConfig.js
+++ support/appConfig.js
@@ -5,7 +5,7 @@ class AppConfig {
     this.baseURL = config.get('baseUrl');
     this.pageTimeOut = config.get('playwrightPageDefaultTimeout');
     this.waitTime = config.get('cucumberTimeout');
-    this.headless = process.env.HEADLESS === 'false'
+    this.headless = process.env.HEADLESS === 'true'
     this.devtools = process.env.DEV_TOOLS === 'true'
     this.env = process.env.NODE_ENV
   }
